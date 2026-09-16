700fc6c6cf579953827fd920c4a0cfec174f8115
Apex <robsonlawson3@gmail.com>
2026-09-17 00:59:06 +0100

    Fix version range issues

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,7 +21,7 @@ dependencyResolutionManagement {
     versionCatalogs.create("libs") {
         version("neoforge", "26.3.0.0-beta")
 
-        library("ghostrenderer", "dev.apexstudios", "ghostrenderer").version("26.3.2-beta-pr-2")
+        library("ghostrenderer", "dev.apexstudios", "ghostrenderer").version("26.3.4-beta-pr-2")
     }
 }
 
